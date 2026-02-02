821e43ec0573f9e6a8516b5b5815e4431428ad43
megan <megand0905@gmail.com>
2026-02-01 18:24:36 -0800

    Sign-in generates JWT

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -1,7 +1,15 @@
 import Post from "../model/Post.js";
 import User from "../model/User.js";
 
-export const getPosts = async(req, res) => {
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const getPosts = async(req, res) => { // GET "/posts"
     try {
         const posts = await Post.find({});
         res.json(posts);
@@ -11,7 +19,7 @@ export const getPosts = async(req, res) => {
     }
 }
 
-export const createPost = async(req, res) => {
+export const createPost = async(req, res) => { // POST "/upload"
     try {
         const { author, description, img, date } = req.body;
         const newPost = new Post({
@@ -31,21 +39,33 @@ export const createPost = async(req, res) => {
     }
 }
 
-export const getUser = async(req, res) => {
+export const getUser = async(req, res) => { // POST "sign-in"
     try {
         const {email, password} = req.body;
         const user = await User.findOne({ email });
 
-        if (user) {
-            if (user.password === password) {
-                res.json("Success");
-            } else {
-                res.status(400).json({message: "Password is incorrect" });
-            } 
-        } else {
+        if (!user) {
             console.log("User not found")
-            res.status(400).json({message: "User does not exist." });
+            return res.status(400).json({message: "User does not exist." });
         }
+
+        const is_correct = await bcrypt.compare(password, user.password);
+
+        if (!is_correct) {
+            return res.status(400).json({message: "Password is incorrect" });
+        }
+        
+        const token = jwt.sign(
+            {
+                email: user.email,
+                id: user._id
+            },
+            JWT_SECRET,
+            { expiresIn: "1h" }
+        );
+
+        res.json({ token });
+        console.log("Generated JWT successfully!");
 
     } catch (err) {
         console.log(err);
@@ -53,15 +73,23 @@ export const getUser = async(req, res) => {
     }
 }
 
-export const createUser = async(req, res) => {
+export const createUser = async(req, res) => { // POST "/sign-up"
     try {
-        const newUser = new User(req.body);
-        const { email } = newUser;
+        const { username, email, password } = req.body;
 
         const userExists = await User.findOne({ email });
         if (userExists) {
             return res.status(400).json({message: "User already exists." });
         }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const newUser = new User({
+            username: username,
+            email: email,
+            password: hashedPassword
+        });
         
         const savedData = await newUser.save();
         res.status(201).json(savedData);
